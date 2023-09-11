8b5e9cac0854817f53cb2df4a1febe8b92aa205f
Logan <logankrieger317@gmail.com>
2023-09-11 16:31:36 -0500

    Act Two Complete

--- act2.sh
+++ act2.sh
@@ -1,13 +1,16 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND using &&) create a file called princess_leia.txt with the text "Help me, Obi-Wan…"
-
+echo "Help me, Obi-Wan..." > princess_leia.txt
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+touch obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+touch luke_skywalker.txt 
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+ touch han_solo.txt chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+mv luke_skywalker.txt millenium_falcon 
+mv obi_wan.txt millenium_falcon
+mv princess_leia.txt millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv millenium_falcon /Users/logan/desktop/GA/unit1/week1/monday/u1_star_wars_hw/star_wars/empire/death_star
